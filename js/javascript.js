6e919c3cd0df915aa3d6807ca1586fb5d9e9e9d6
barisdongel <barisdng26@gmail.com>
2020-04-04 17:23:52 +0300

    testimonials section modify and added partners

--- js/javascript.js
+++ js/javascript.js
@@ -57,41 +57,76 @@ window.onload = function() {
 };
 
 $(document).ready(function(){
-
-if($('.brands_slider').length)
-{
-var brandsSlider = $('.brands_slider');
-
-brandsSlider.owlCarousel(
-{
-loop:true,
-autoplay:true,
-autoplayTimeout:5000,
-nav:false,
-dots:false,
-autoWidth:true,
-items:8,
-margin:42
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
 });
+jQuery(document).ready(function($) {
 
-if($('.brands_prev').length)
-{
-var prev = $('.brands_prev');
-prev.on('click', function()
-{
-brandsSlider.trigger('prev.owl.carousel');
-});
-}
+  var feedbackSlider = $('.feedback-slider');
+  feedbackSlider.owlCarousel({
+    items: 1,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+    responsive:{
 
-if($('.brands_next').length)
-{
-var next = $('.brands_next');
-next.on('click', function()
-{
-brandsSlider.trigger('next.owl.carousel');
-});
-}
-}
+      // breakpoint from 767 up
+      767:{
+        nav: true,
+        dots: false
+      }
+    }
+  });
 
+  feedbackSlider.on("translate.owl.carousel", function(){
+    $(".feedback-slider-item h3").removeClass("animated fadeIn").css("opacity", "0");
+    $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating").removeClass("animated zoomIn").css("opacity", "0");
+  });
 
-});
+  feedbackSlider.on("translated.owl.carousel", function(){
+    $(".feedback-slider-item h3").addClass("animated fadeIn").css("opacity", "1");
+    $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating").addClass("animated zoomIn").css("opacity", "1");
+  });
+  feedbackSlider.on('changed.owl.carousel', function(property) {
+    var current = property.item.index;
+    var prevThumb = $(property.target).find(".owl-item").eq(current).prev().find("img").attr('src');
+    var nextThumb = $(property.target).find(".owl-item").eq(current).next().find("img").attr('src');
+    var prevRating = $(property.target).find(".owl-item").eq(current).prev().find('span').attr('data-rating');
+    var nextRating = $(property.target).find(".owl-item").eq(current).next().find('span').attr('data-rating');
+    $('.thumb-prev').find('img').attr('src', prevThumb);
+    $('.thumb-next').find('img').attr('src', nextThumb);
+    $('.thumb-prev').find('span').next().html(prevRating + '<i class="fa fa-star"></i>');
+    $('.thumb-next').find('span').next().html(nextRating + '<i class="fa fa-star"></i>');
+  });
+  $('.thumb-next').on('click', function() {
+    feedbackSlider.trigger('next.owl.carousel', [300]);
+    return false;
+  });
+  $('.thumb-prev').on('click', function() {
+    feedbackSlider.trigger('prev.owl.carousel', [300]);
+    return false;
+  });
+  
+}); //end ready
