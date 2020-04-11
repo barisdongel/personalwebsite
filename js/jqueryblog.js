b767812e5e1bdb7dae6430188ad767d4c68a51f4
barisdongel <barisdng26@gmail.com>
2020-04-11 15:05:48 +0300

    blog page javascript

--- js/jqueryblog.js
+++ js/jqueryblog.js
@@ -1,7 +1,19 @@
-   $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40barisdng26" , function(data){
-  	$.each(data, function(key,value){
-  		var row="";
-  		row +=value.feed;
-  		$("#title").append(row);
-  	})
-  });
+const posts = ["https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@barisdng26"];
+
+function posttamplate(post) {
+  return `
+            <div class="jumbotron">
+              <img src="${post.thumbnail}">
+              <h1 id="title" class="display-4">${post.title}</h1>
+              <p>${post.description}</p>
+              <hr class="my-4">
+              <p>${post.pubDate}</p>
+              <a class="btn btn-info btn-lg" href="#" role="button">Yazıyı oku...</a>
+            </div>
+  `;
+}
+
+
+document.getElementById("app").innerHTML = `
+  ${posts.map(posttamplate).join("")}
+`;
