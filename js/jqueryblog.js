d5072d311e22544d8e30cb14a16cb1bbb59236a4
barisdongel <barisdng26@gmail.com>
2020-04-12 00:14:13 +0300

    blog post get from medium

--- js/jqueryblog.js
+++ js/jqueryblog.js
@@ -1,19 +1,61 @@
-const posts = ["https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@barisdng26"];
+let my_data, my_author, my_description, my_items, my_link, my_pubDate;
 
-function posttamplate(post) {
-  return `
-            <div class="jumbotron">
-              <img src="${post.thumbnail}">
-              <h1 id="title" class="display-4">${post.title}</h1>
-              <p>${post.description}</p>
-              <hr class="my-4">
-              <p>${post.pubDate}</p>
-              <a class="btn btn-info btn-lg" href="#" role="button">Yazıyı oku...</a>
-            </div>
-  `;
-}
+$.ajax({
+    url: "https://api.rss2json.com/v1/api.json",
+    data: {"rss_url": "https://medium.com/feed/@barisdng26"},
+    success: function (data) {
+        my_data = data;
+        my_author = my_data["feed"]["author"]; // It returns blank, you should define this in medium i think
+        my_items = my_data["items"];
+
+        for(let i = 0; i < my_items.length; i++) {
+            let my_item = my_items[i];
+            let my_item_title = my_item["title"]; // get items author
+            let my_item_pubDate = my_item["pubDate"]; // get items author
+            let my_item_description = my_item["description"]; // get items author
+            let my_item_link = my_item["link"]; // get items author
+
+            // create item
+            let my_item_div = document.createElement('div');
+            my_item_div.setAttribute("id", "item" + i.toString());
+
+            // append title to item div
+            let my_item_title_div = document.createElement('div');
+            my_item_title_div.setAttribute("id", "author" + i.toString());
+
+            // append pubDate to item div
+            let my_item_pubDate_div = document.createElement('div');
+            my_item_pubDate_div.setAttribute("item" + i.toString(), "pubDate" + i.toString());
+
+            // append description to item div
+            let my_item_description_div = document.createElement('div');
+            my_item_description_div.setAttribute("item" + i.toString(), "description" + i.toString());
+
+            // append link to item div
+            let my_item_link_div = document.createElement('div');
+            my_item_link_div.setAttribute("item" + i.toString(), "link" + i.toString());
+            
+            // change title content of the item
+            my_item_title_div.innerHTML = "<h1 class='card-title'>" + my_item_title + "</h1>";
+
+            // change pubDate content of the item
+            my_item_pubDate_div.innerHTML ="<h5 class='pt-4 pb-4 blockquote-footer'> Yayın Tarihi: " + my_item_pubDate + "</h5>";  
+            
+            // change description content of the item
+            my_item_description_div.innerHTML = "<p class='card-text'>"+my_item_description;+"</p>"
+
+            // change link content of the item
+            my_item_link_div.innerHTML ="<a class='btn btn-info p-2' href='"+my_item_link+"'>Yazıyı Oku</a><hr>";  
+
+            document.getElementById("items").appendChild(my_item_div);
+            document.getElementById("item" + i.toString()).appendChild(my_item_title_div);
+            document.getElementById("item" + i.toString()).appendChild(my_item_description_div);
+            document.getElementById("item" + i.toString()).appendChild(my_item_pubDate_div);
+            document.getElementById("item" + i.toString()).appendChild(my_item_link_div);
+
+        }
 
 
-document.getElementById("app").innerHTML = `
-  ${posts.map(posttamplate).join("")}
-`;
+        // console.log(data);
+    }
+});
